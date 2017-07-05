644a2e95aa8d6aa603d87de0ae4613cc650734d6
Wilson Yu <hello@wilsonyu.io>
2017-07-04 23:50:38 -0400

    add css colors for demonstration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,39 @@ import {Tab} from './tab';
   selector: 'app-root',
   template: `
     <tabs>
-      <tab [tabTitle]="'Tab 1'">
-        Tab 1 Content
-        <p>TAB ONE!!!!</p>
+      <tab class="white" tabTitle="WHITE">
+        <div class="white">
+          <p class="whiteText">WHITE!</p>
+          <p>CONTENT GOES HERE</p>
+        </div>
       </tab>
-      <tab tabTitle="Tab 2">
-        Tab 2 Content
-        <p>TAB TWOOOOOOO!!!!</p>
+      <tab class="blue" tabTitle="BLUE">
+        <div class="blue">
+          <p class="blueText">BLUE!</p>
+          <p>CONTENT GOES HERE</p>
+        </div>
+      </tab>
+      <tab class="black" tabTitle="BLACK">
+        <div class="black">
+          <p class="blackText">BLACK!</p>
+          <p>CONTENT GOES HERE</p>
+        </div>
+      </tab>
+      <tab class="red" tabTitle="RED">
+        <div class="red">
+          <p class="redText">RED!</p>
+          <p>CONTENT GOES HERE</p>
+        </div>
+      </tab>
+      <tab class="green" tabTitle="GREEN">
+        <div class="green">
+          <p class="greenText">GREEN!</p>
+          <p>CONTENT GOES HERE</p>
+        </div>
       </tab>
     </tabs>
   `,
-  styles: ['']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
