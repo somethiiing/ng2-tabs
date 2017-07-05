a76534a1e3c92c364c3bbf8f8b4840525c062d04
Wilson Yu <hello@wilsonyu.io>
2017-07-05 00:07:18 -0400

    fix styling

--- src/app/tabs.ts
+++ src/app/tabs.ts
@@ -6,10 +6,10 @@ import { Tab } from './tab';
   template: `
     <ul class="nav nav-tabs">
       <li *ngFor="let tab of tabs" (click)="selectTab(tab)" [class.active]="tab.active">
-        <p>{{tab.title}}</p>
+        <a>{{tab.title}}</a>
       </li>
     </ul>
-    <ng-content></ng-content>
+    <ng-content>
   `
 })
 export class Tabs implements AfterContentInit {
